1b5134ab4da6d766393f7ba1c7c9d75f07c8d4d8
temitope00 <81693367+temitope00@users.noreply.github.com>
2022-07-31 08:11:09 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const aboutMe = {
+    name: 'Ajayi Temitope Grace',
+    height: '5.7"',
+    country: 'Nigeria'
+}
+
+document.write(
+    aboutMe.name,
+    aboutMe.height,
+    aboutMe.country
+)
